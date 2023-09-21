18fb7ff5bba211564de59f7db726e6c3ecbf478f
Yash Chowdhari <yashc1999@gmail.com>
2023-09-21 09:46:04 +0530

    feat: My first feature

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -28,7 +28,7 @@ const navigation = [
 const teams = [
     {name: 'Engineering', href: '#', bgColorClass: 'bg-indigo-500'},
     {name: 'Human Resources', href: '#', bgColorClass: 'bg-green-500'},
-    {name: 'Customer Success', href: '#', bgColorClass: 'bg-yellow-500'},
+    {name: 'Customer Success', href: '#', bgColorClass: 'bg-yellow-500'},ShriKrishnaCommercial/skc_new.git'
 ]
 const projects = [
     {
