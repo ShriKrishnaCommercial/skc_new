36213cdab25df240528c0d50fff21448be1f22b6
Mihir Sawant <mihirssawant06@gmail.com>
2024-03-13 07:34:32 +0530

    dashboard work done and cv route and ckeditor added
    basic work is done of adding validations will check

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,42 +4,67 @@ import DashboardHeader from "@/components/Dashboard/header";
 import React, {useEffect, useState} from "react";
 import dynamic from "next/dynamic";
 import axios from "axios";
+import {useRouter} from "next/navigation";
+import {toast} from "react-toastify";
 const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
     ssr: false,
 });
+
 export default function dashboard() {
-    const [count, setCount] = useState({
-        product: 0, subProduct: 0, brand: 0
-    });
+    const [prodCount, setProdCount] = useState(0);
+    const [subProdCount, setSubProdCount] = useState(0);
+    const [cvCount, setCvCount] = useState(0);
+    const [brandCount, setBrandCount] = useState(0);
 
     const [people, setPeople] = useState([]);
+
+    const router = useRouter();
+
+    const token = sessionStorage.getItem("jwt");
+
+    if(!token){
+        router.push("/dashboard/login");
+        toast.error("Session Expired !", {
+            position: 'top-right',
+            autoClose: 3000,
+            closeOnClick: true
+        });
+    }
+
 
     useEffect(() => {
         axios.get('/api/dashboard/counts', {
             headers: {
                 'Content-Type' : 'application/json',
-                'Authorization' : 'Bearer '+sessionStorage.getItem("jwt")
+                'Authorization' : 'Bearer '+token
             }
         }).then(res => {
-            setCount(res.data.allCounts)
-            console.log(count)
+            setProdCount(res.data.products);
+            setSubProdCount(res.data.productsCategory);
+            setCvCount(res.data.cvs);
+            setBrandCount(res.data.brands);
         })
             .catch(err => console.error(err))
     }, []);
 
     const stats = [
-        // {name: 'Blogs', value: '$405,091.00', change: '+4.75%', changeType: 'positive'},
-        {name: 'Products', value: count.product, change: '+54.02%', changeType: 'negative'},
-        {name: 'Products Category', value: count.subProduct, change: '-1.39%', changeType: 'positive'},
-        // {name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative'},
+        {name: 'Products', value: prodCount, change: '+54.02%', changeType: 'negative'},
+        {name: 'Products Category', value: subProdCount, change: '-1.39%', changeType: 'positive'},
+        {name: 'Brands', value: brandCount, change: '+4.75%', changeType: 'positive'},
+        {name: 'CVs', value: cvCount, change: '+10.18%', changeType: 'negative'}
     ]
-    axios.get('/api/dashboard/cvs/list', {
-        headers: {
-            'Content-Type' : 'application/json',
-            'Authorization' : 'Bearer '+sessionStorage.getItem("jwt")
-        }
-    }).then(res => setPeople(res.data.cvs))
-        .catch(err => console.error(err))
+    useEffect(() => {
+        axios.get('/api/dashboard/cvs/list', {
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization' : 'Bearer '+token
+            }
+        }).then(res => {
+            setPeople(res.data.cvs)
+            console.log(res.data)
+        })
+            .catch(err => console.error(err))
+    }, []);
 
     function classNames() {
         // return classes.filter(Boolean).join(' ')
@@ -71,15 +96,15 @@ export default function dashboard() {
                                             className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8"
                                         >
                                             <dt className="text-sm font-medium leading-6 text-gray-500">{stat.name}</dt>
-                                            <dd
-                                                className={stat.changeType === 'negative' ? 'text-rose-600' : 'text-gray-700 ' + 'text-xs font-medium'}
-                                            >
-                                                {stat.change}
-                                            </dd>
+                                            {/*<dd*/}
+                                            {/*    className={stat.changeType === 'negative' ? 'text-rose-600' : 'text-gray-700 ' + 'text-xs font-medium'}*/}
+                                            {/*>*/}
+                                            {/*    {stat.change}*/}
+                                            {/*</dd>*/}
                                             <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
                                                 <AnimatedNumbers
                                                     includeComma
-                                                    animateToNumber={stat.value}
+                                                    animateToNumber={Number(stat.value)}
                                                     fontStyle={{fontSize: 40, fontWeight: "black"}}
                                                     locale="en-US"
                                                     configs={[
@@ -95,36 +120,26 @@ export default function dashboard() {
                                         </div>
                                     ))}
                                 </dl>
-                                {/*<ul role="list" className="divide-y divide-gray-100">*/}
-                                {/*    {people.map((person) => (*/}
-                                {/*        <li key={person._id} className="flex justify-between gap-x-6 py-5">*/}
-                                {/*            <div className="flex gap-x-4">*/}
-                                {/*                /!*<img className="h-12 w-12 flex-none rounded-full bg-gray-50"*!/*/}
-                                {/*                /!*     src={person.imageUrl} alt=""/>*!/*/}
-                                {/*                <div className="min-w-0 flex-auto">*/}
-                                {/*                    <p className="text-sm font-semibold leading-6 text-gray-900">{person.first_name}</p>*/}
-                                {/*                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>*/}
-                                {/*                </div>*/}
-                                {/*            </div>*/}
-                                {/*            /!*<div className="hidden sm:flex sm:flex-col sm:items-end">*!/*/}
-                                {/*            /!*    <p className="text-sm leading-6 text-gray-900">{person.role}</p>*!/*/}
-                                {/*            /!*    {person.lastSeen ? (*!/*/}
-                                {/*            /!*        <p className="mt-1 text-xs leading-5 text-gray-500">*!/*/}
-                                {/*            /!*            Last seen <time*!/*/}
-                                {/*            /!*            dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>*!/*/}
-                                {/*            /!*        </p>*!/*/}
-                                {/*            /!*    ) : (*!/*/}
-                                {/*            /!*        <div className="mt-1 flex items-center gap-x-1.5">*!/*/}
-                                {/*            /!*            <div className="flex-none rounded-full bg-emerald-500/20 p-1">*!/*/}
-                                {/*            /!*                <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"/>*!/*/}
-                                {/*            /!*            </div>*!/*/}
-                                {/*            /!*            <p className="text-xs leading-5 text-gray-500">Online</p>*!/*/}
-                                {/*            /!*        </div>*!/*/}
-                                {/*            /!*    )}*!/*/}
-                                {/*            /!*</div>*!/*/}
-                                {/*        </li>*/}
-                                {/*    ))}*/}
-                                {/*</ul>*/}
+                                <ul role="list" className="divide-y divide-gray-100">
+                                    {people.map((person) => (
+                                        <li key={person._id} className="flex justify-between gap-x-6 py-5">
+                                            <div className="flex gap-x-4">
+                                                {/*<img className="h-12 w-12 flex-none rounded-full bg-gray-50"*/}
+                                                {/*     src={person.imageUrl} alt=""/>*/}
+                                                <div className="min-w-0 flex-auto">
+                                                    <p className="text-sm font-semibold leading-6 text-gray-900">{person.firs_name + ' ' + person.last_name }</p>
+                                                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                                                </div>
+                                            </div>
+                                            <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                                <p className="text-sm leading-6 text-gray-900">{String(person.country).toUpperCase()}</p>
+                                                    <div className="mt-1 flex items-center gap-x-1.5">
+                                                        <p className="text-xs leading-5 text-gray-500">{person.file}</p>
+                                                    </div>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
                         </div>
                     </div>
