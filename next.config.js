04cc7794e85d7e44db87cb486006a3373d3cdc5f
ShriKrishnaCommercial <141113709+ShriKrishnaCommercial@users.noreply.github.com>
2024-08-23 10:35:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com','tailwindui.com','opendoodles.s3-us-west-1.amazonaws.com','flowbite.s3.amazonaws.com']
     },
-    eslint :{
+    eslint : {
         ignoreDuringBuilds: true,
-    }
+    },
+    typescript : {
+        ignoreBuildErrors : true
+        }
 }
 
 module.exports = nextConfig
